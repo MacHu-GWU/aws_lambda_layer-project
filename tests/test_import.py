06cbbc880db75619a9f602aab6b11409d14d8db2
Sanhe Hu <MacHu-GWU@users.noreply.github.com>
2023-05-27 16:02:14 -0400

    fix test

--- tests/test_import.py
+++ tests/test_import.py
@@ -1,12 +1,23 @@
 # -*- coding: utf-8 -*-
 
 import pytest
-from pytest import raises, approx
 
 
 def test():
-    import aws_lambda_layer
-    pass
+    from aws_lambda_layer import api
+
+    _ = api.BuildContext
+    _ = api.get_latest_layer_version
+    _ = api.is_current_layer_the_same_as_latest_one
+    _ = api.build_layer_artifacts
+    _ = api.upload_layer_artifacts
+    _ = api.publish_layer
+    _ = api.deploy_layer
+    _ = api.LayerDeployment
+    _ = api.build_source_artifacts
+    _ = api.upload_source_artifacts
+    _ = api.publish_source_artifacts
+    _ = api.SourceArtifactsDeployment
 
 
 if __name__ == "__main__":
